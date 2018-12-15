195de7bd05cbe87da5560cfea76baf74d78a0ccb
Kensuke Machii <kensukemachii@gmail.com>
2018-12-15 13:48:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-// expressの呼び込み
-const express = require('express');
-// PORTの設定
-// もしenvにportが確立したらそのポートを、していなかったら8080を使う
-const port = process.env.PORT || 8080;
-// express()のインスタンス
-const app = express();
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-// どこにアクセスさせるかの設定
-// distフォルダ：コンパイルされたファイルが入る場所
-app.use(express.static(__dirname + "/dist/"));
+const app = express()
 
-// SPAの時、ルーティングがうまくいかない時があるので以下の設定
-// これをしていないと、https://~~~/aboutとかに行った時にリロードするとエラーがでる
-// SPAだとaboutファイルを直で読み込んでいないから
-// これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
-// /.*/で全てのルートを指定。req(request), res(response)
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// 一番上で指定したportをlisten
-app.listen(port);
+const port = process.env.PORT || 8080
+app.listen(port)
 
-// 動いてるかどうかconsoleで見る
-console.log("Server is up!!");
+console.log('Listening on port: ' + port)
