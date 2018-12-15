45821137a339cee835b2e6caa8bc42e78e532637
Kensuke Machii <kensukemachii@gmail.com>
2018-12-15 12:03:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
- var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
- var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+constat express = require("express");
+constat port = process.env.PORT || 8080;
+constat app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
+app.listen(port);
+
+console.log("Server started...");
